1ffd024094eb511bd8f627f19afad6c7590a9803
AhuraMagda <lubaszka.magda@gmail.com>
2023-04-10 11:45:21 +0200

    map changes after refreshing the page

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 
 // Adding a map to the page
-var map = L.map('map').setView([51.505, -0.09], 13);
+// var map = L.map('map').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+let mapLat = 51.505;
+let mapLng = -0.09;
+
 
 
 // My api key and given IP
 
 const apiKey = "at_Kj8jaalEUx3dQLHFZI3jj2yeer6BU";
-let ip = "211.14.214.5"; 
-let apiToCall = "https://geo.ipify.org/api/v2/country?apiKey=" + apiKey + "&ipAddress=" + ip;
+let ip = "68.103.99.97"; 
+ip = localStorage.getItem("newIp") //musi być na zewnątrz, żeby to działało po odświeżeniu bo tak to nie wywoła funkcji
+let apiToCall = "https://geo.ipify.org/api/v2/country,city,vpn?apiKey=" + apiKey + "&ipAddress=" + ip;
 console.log(apiToCall)
 
 // Get input
 
 
 const getIpFromInput = () => {
-  ip = document.getElementById('given-ip-input').value
-  apiToCall = "https://geo.ipify.org/api/v2/country?apiKey=" + apiKey + "&ipAddress=" + ip;
+  newIp = document.getElementById('given-ip-input').value
+  localStorage.setItem("newIp", newIp) //zapisujemy nowe ip w pamięci
+  ip = localStorage.getItem("newIp") //nadpisywanie?
+  apiToCall = "https://geo.ipify.org/api/v2/country,city?apiKey=" + apiKey + "&ipAddress=" + ip;
   changeValue()  
 }
 
@@ -50,7 +52,30 @@ async function changeValue() {
   ipAdress.innerHTML = data.ip
   ipLocation.innerHTML = data.location.region
   ipTimezone.innerHTML = "UTC " + data.location.timezone
-  ipIsp.innerHTML = data.ispgirb
+  ipIsp.innerHTML = data.isp
+  changeMap(data.location.lat, data.location.lng)
+  setMap()
+}
+
 changeValue()
 
-// random ip 211.14.214.5
+
+// changing the map
+
+
+
+const changeMap = (newLat, newLng) => {
+  mapLat = newLat;
+  mapLng = newLng;
+  console.log("map lat: " + mapLat)
+}
+
+
+const setMap = () => {
+  let map = L.map('map').setView([mapLat, mapLng], 17);
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+}
